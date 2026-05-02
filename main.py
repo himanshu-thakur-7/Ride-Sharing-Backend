f692d73c13dcd0e61d726cf959d2e78b2e99a8df
Himanshu <himan7781@gmail.com>
2026-05-02 13:09:24 +0530

    Added request ride endpoint

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import uuid
 
 app = FastAPI()
+
+# In memory storage (temporary)
+rides = {}
+
+class RideRequest(BaseModel):
+    pickup:str
+    destination:str
+
 
 @app.get("/")
 def home():
     return {"message":"Uber backend starting...."}
+
+@app.post("/rides")
+def create_ride(request: RideRequest):
+    ride_id = str(uuid.uuid4())
+
+    ride = {
+        "Id" : ride_id,
+        "pickup": request.pickup,
+        "destination": request.destination,
+        "status":"REQUESTED"
+    }
+
+    rides[ride_id] = ride
+
+    return ride
